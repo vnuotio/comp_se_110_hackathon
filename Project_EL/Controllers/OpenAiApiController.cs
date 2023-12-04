20ade2fa649122ac4af3760d576dd2725100a281
Vertti Nuotio <vertti.nuotio@gmail.com>
2023-12-04 13:07:39 +0200

    add OpenAiApiController and basic query function

---
+++ Project_EL/Controllers/OpenAiApiController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using OpenAI_API;
+using System.Threading.Tasks;
+
+namespace Project_EL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OpenAiApiController : ControllerBase
+    {
+        OpenAIAPI api;
+        public OpenAiApiController()
+        {
+            using (var sr = new StreamReader("apikey.txt"))
+            {
+                api = new OpenAIAPI(sr.ReadLine());
+                sr.Close();
+            }
+        }
+
+        public async Task<string> GetQueryResult(string query)
+        {
+            var chat = api.Chat.CreateConversation();
+            chat.AppendUserInput(query);
+            return await chat.GetResponseFromChatbotAsync();
+        }
+    }
+}
